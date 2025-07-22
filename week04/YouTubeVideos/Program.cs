e737712596ac66d18359745ffef68b45e06022be
Fireking1996 <firebringer1996@yahoo.com>
2025-07-21 21:30:57 -0500

    update

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,58 +1,6 @@
 using System;
 using System.Collections.Generic;
 
-class Comment
-{
-    public string CommenterName { get; set; }
-    public string Text { get; set; }
-
-    public Comment(string commenterName, string text)
-    {
-        CommenterName = commenterName;
-        Text = text;
-    }
-}
-
-class Video
-{
-    public string Title { get; set; }
-    public string Author { get; set; }
-    public int LengthInSeconds { get; set; }
-
-    private List<Comment> comments = new List<Comment>();
-
-    public Video(string title, string author, int lengthInSeconds)
-    {
-        Title = title;
-        Author = author;
-        LengthInSeconds = lengthInSeconds;
-    }
-
-    public void AddComment(Comment comment)
-    {
-        comments.Add(comment);
-    }
-
-    public int GetCommentCount()
-    {
-        return comments.Count;
-    }
-
-    public void DisplayVideoInfo()
-    {
-        Console.WriteLine($"Title: {Title}");
-        Console.WriteLine($"Author: {Author}");
-        Console.WriteLine($"Length: {LengthInSeconds} seconds");
-        Console.WriteLine($"Number of Comments: {GetCommentCount()}");
-        Console.WriteLine("Comments:");
-        foreach (var comment in comments)
-        {
-            Console.WriteLine($" - {comment.CommenterName}: {comment.Text}");
-        }
-        Console.WriteLine();
-    }
-}
-
 class Program
 {
     static void Main(string[] args)
